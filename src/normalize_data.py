312123d93032fe684118ef3ef7606d35e7e203e1
yhadevol <yh_adevol@hotmail.com>
2026-02-05 13:17:05 -0500

    add norm for mouse ileum

--- src/normalize_data.py
+++ src/normalize_data.py
@@ -18,3 +18,13 @@ sc.tl.pca(adata, n_comps=50, use_highly_variable=True)
 sc.pp.neighbors(adata)
 sc.tl.umap(adata)
 adata.write('../data/mouse_colon/mouse_colon_normed.h5ad')
+
+adata = sc.read('../data/mouse_ileum/mouse_ileum_cnt.h5ad')
+adata.var['entrez'] = adata.var_names
+sc.pp.normalize_total(adata, target_sum=1e4)
+sc.pp.log1p(adata)
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+sc.tl.pca(adata, n_comps=50, use_highly_variable=True)
+sc.pp.neighbors(adata)
+sc.tl.umap(adata)
+adata.write('../data/mouse_ileum/mouse_ileum_normed.h5ad')
