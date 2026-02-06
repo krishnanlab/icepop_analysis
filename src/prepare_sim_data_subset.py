1a1a73743642a060aa6cdf68c5b968093a2d6dc6
yhadevol <yh_adevol@hotmail.com>
2026-02-06 16:07:22 -0500

    prepare simulation dataset

--- src/prepare_sim_data_subset.py
+++ src/prepare_sim_data_subset.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
     min_cells = 10
     min_cell_in_gene = 10
 
-    outdir = '../data/simulation_input_subset'
+    outdir = '../data/simulation'
     Path(outdir).mkdir(exist_ok=True)
 
     # load data
-    adata = sc.read('../data/sc_data/TM_FACS_MetaQ.h5ad')
+    adata = sc.read('../data/TM_FACS/TM_FACS_cnt.h5ad')
 
     # randomizer
     rng = np.random.default_rng(1)
@@ -38,34 +38,24 @@ if __name__ == '__main__':
     adata_sub = adata_sub[:, genes_keep].copy()
 
     # get count and convert across species
-    score_converter = CrossSpeciesScoreConverter(adata_sub, sp='10090')
+    score_converter = CrossSpeciesScoreConverter(adata_sub)
     score_converter.generate_cross_sp_matrix()
     exp = score_converter.convert_score_across_species(
         pd.DataFrame(
-            adata_sub.layers['raw'].toarray(),
+            adata_sub.X.toarray(),
             index=adata_sub.obs_names,
-            columns=adata_sub.var['entrez']),
+            columns=adata_sub.var_names
+        ),
         normed=False,
     )
+
     # make new adata
     X = csr_matrix(np.asarray(exp, dtype=np.float32))
-    varnames = pd.DataFrame(zip(
-        exp.columns,
-        exp.columns),
-        columns=['name', 'entrez']
-    )
-    varnames = varnames.set_index('name')
     new_adata = ad.AnnData(
         X=X,              # matrix (cells × genes)
         obs=adata_sub.obs,      # keep same metadata
-        var=varnames
+        var=pd.DataFrame(index=exp.columns)
     )
-    new_adata.obs = new_adata.obs.drop(columns=['SEACell'])
-    new_adata.layers['raw'] = X.copy()
 
-    # preprocess
-    sc.pp.normalize_total(new_adata, target_sum=1e4)
-    sc.pp.log1p(new_adata)
-    sc.pp.highly_variable_genes(new_adata, n_top_genes=2000)
-    sc.tl.pca(new_adata, n_comps=50, mask_var="highly_variable")
+    # save
     new_adata.write(f'{outdir}/TM_subset.h5ad')
